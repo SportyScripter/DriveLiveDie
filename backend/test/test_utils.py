4cbbf41e518957cb2202a6dc1ccdb549a4140f22
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2024-03-23 16:03:35 +0100

    ref.

--- backend/test/test_utils.py
+++ backend/test/test_utils.py
@@ -1,18 +1,9 @@
-from auth.utils import (
-    get_hashed_password,
-    password_context,
-    verify_password,
-    create_access_token,
-    create_refresh_token,
-    get_user,
-    get_current_active_user,
-    get_current_user
-)
+from auth import utils
 import unittest
 
 
 def test_get_hashed_password():
     test_password = "superSecret123"
-    hashed_password = get_hashed_password(test_password)
-    assert password_context.verify(test_password, hashed_password), "The hashed password does not match the original password."
+    hashed_password = utils.get_hashed_password(test_password)
+    assert utils.password_context.verify(test_password, hashed_password), "The hashed password does not match the original password."
 
