87af6f613f333adafb91de8bf0d8a5ab64ef9251
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2024-03-26 18:37:44 +0100

    fix.github action

--- backend/core/config.py
+++ backend/core/config.py
@@ -11,7 +11,7 @@ class Settings:
     POSTGRES_USER : str = os.getenv("POSTGRES_USER", "default_user")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "default_pass")
     POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT", "5432")  
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT", "5433")  
     POSTGRES_DB : str = os.getenv("POSTGRES_DB", "default_db")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     
