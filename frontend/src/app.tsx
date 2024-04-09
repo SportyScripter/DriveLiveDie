84e62b7288e7ab2613652bba5cf2faf46248aceb
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2024-04-09 22:52:10 +0200

    docker & ui changes

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,11 +2,19 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './index.css'
+
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
+import { AuthProvider, useAuth } from './auth'
+
+
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, defaultPreload: 'intent',
+  context: {
+    auth: undefined!, // This will be set after we wrap the app in an AuthProvider
+  }, })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -15,14 +23,21 @@ declare module '@tanstack/react-router' {
   }
 }
 
+function InnerApp() {
+  const auth = useAuth()
+  return <RouterProvider router={router} context={{ auth }} />
+}
+
 // Render the app
 const rootElement = document.getElementById('root')!
 
 if (!rootElement.innerHTML) {
+  
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
+<AuthProvider>
+      <InnerApp />
+    </AuthProvider>    </StrictMode>,
   )
 }
--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -6,8 +6,17 @@ export const Route = createLazyFileRoute('/')({
 
 export function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <>
+    <header className="bg-white shadow">
+    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
     </div>
+  </header>
+  <main>
+    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+     Hello from deploy
+    </div>
+  </main>
+  </>
   )
 }
